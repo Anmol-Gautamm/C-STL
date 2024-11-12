348aceec4844db6569621d9ab642b2629874f0fc
Anmol Gautam <anmolgautam1203.ag@gmail.com>
2024-11-12 18:09:44 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -189,6 +189,8 @@ void explainstack(){
 
     cout<<st.size();
     // here it will check the size and the empty of the stack
+    // cout<<st.top;
+    // here it will print the top element of the stack
 
 
 
